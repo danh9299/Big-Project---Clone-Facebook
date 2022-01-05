0df0d34975f0b6415790feff86e431541b9bc113
ThaiNguyennd <thaittn01@gmail.com>
2022-01-05 21:19:18 +0700

    fix mess

---
+++ FB-messenger/script.js
@@ -0,0 +1,35 @@
+// function openNav() {
+//   if (document.getElementById("main").style.display == "none") {
+//     document.getElementById("main").style.display = "block";
+//   } else {
+//   }
+// }
+// function openNav() {
+//   var open = document.getElementById("mySidebar");
+//   open.style.width = "200px";
+// }
+// function closeNav() {
+//   var close = document.getElementById("mySidebar");
+//   close.style.width = "0px";
+// }
+function appear1() {
+  if (document.getElementById("menu1").style.display == "none") {
+    document.getElementById("menu1").style.display = "block";
+  } else {
+    document.getElementById("menu1").style.display = "none";
+  }
+}
+function appear2() {
+  if (document.getElementById("menu2").style.display == "none") {
+    document.getElementById("menu2").style.display = "block";
+  } else {
+    document.getElementById("menu2").style.display = "none";
+  }
+}
+function appear3() {
+  if (document.getElementById("menu3").style.display == "none") {
+    document.getElementById("menu3").style.display = "block";
+  } else {
+    document.getElementById("menu3").style.display = "none";
+  }
+}
