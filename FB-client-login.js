df4eeb5d4b40bbab57fcd980ecc8336eb8f9f331
long0167472 <Hoanglong7374@gmail.com>
2022-01-14 03:17:57 +0700

    Add register container

--- FB-client-login.js
+++ FB-client-login.js
@@ -1,52 +1,70 @@
 //Function for checking email
-function checkEmailValidate(email){
+function checkEmailValidate(email) {
     let emailPattern = /.+@.+(.com)$/;
 
     // The email ends with .com
     // There must be string or char (no strict about the type) before @
     // There must be string or char (bo strict about the type) before .com and after @
-    
 
-    if(emailPattern.test(email)){
-        document.getElementById("emailHelp1").textContent='Email đúng định dạng';
-        document.getElementById("emailHelp1").style.color='grey';
+
+    if (emailPattern.test(email)) {
+        document.getElementById("emailHelp1").textContent = 'Email đúng định dạng';
+        document.getElementById("emailHelp1").style.color = 'grey';
         check1 = true;
-    }else{
-        document.getElementById("emailHelp1").textContent='Bạn nhập chưa đúng định dạng Email yêu cầu';
-        document.getElementById("emailHelp1").style.color='red';
-        check1=false;
+    } else {
+        document.getElementById("emailHelp1").textContent = 'Bạn nhập chưa đúng định dạng Email yêu cầu';
+        document.getElementById("emailHelp1").style.color = 'red';
+        check1 = false;
     }
-    
+
 }
 //function for checking password
-function checkPasswordValidate(pass){
+function checkPasswordValidate(pass) {
     let passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     // password has at least (1 upper case, 1 lower case, 1 symbol, 1 number,)
     //the password must contain at least 8 chars 
-    if(passPattern.test(pass)){
-        document.getElementById("passHelp1").textContent='Password đúng định dạng';
-        document.getElementById("passHelp1").style.color='grey';
-        check2=true;
-    }else{
-        document.getElementById("passHelp1").textContent='Tối thiểu tám ký tự, ít nhất một chữ cái viết hoa, một chữ cái viết thường, một số và một ký tự đặc biệt:';
-        document.getElementById("passHelp1").style.color='red';
-        check2=false;
+    if (passPattern.test(pass)) {
+        document.getElementById("passHelp1").textContent = 'Password đúng định dạng';
+        document.getElementById("passHelp1").style.color = 'grey';
+        check2 = true;
+    } else {
+        document.getElementById("passHelp1").textContent = 'Tối thiểu tám ký tự, ít nhất một chữ cái viết hoa, một chữ cái viết thường, một số và một ký tự đặc biệt:';
+        document.getElementById("passHelp1").style.color = 'red';
+        check2 = false;
     }
 }
 // All the element joins the logining process
 let txtEmail = document.getElementById("txtEmail");
-let txtPass  = document.getElementById("txtPass");
+let txtPass = document.getElementById("txtPass");
 let btnLogin = document.getElementById("btnforlogin");
 let check1 = false; //the check1 is to see whether the email is correct with standard of Regex or not
-let check2 = false;//the check2 is to see whether the password is correct with standard of Regex or not
+let check2 = false; //the check2 is to see whether the password is correct with standard of Regex or not
 let loginA = document.getElementById("loginA");
 
 // Function
-btnLogin.addEventListener("click",Func1);
-function Func1(){  
+btnLogin.addEventListener("click", Func1);
+
+function Func1() {
     checkEmailValidate(txtEmail.value);
     checkPasswordValidate(txtPass.value);
     if (check1 == true & check2 == true)
-        loginA.href="FB-client-newFeed.html";
+        loginA.href = "FB-client-newFeed.html";
 }
 
+// Register
+
+
+document.getElementById("Register").addEventListener("click", function() {
+    document.getElementById("Register-container").style.display = 'block';
+    document.getElementById("login-page").style.opacity = '0.3';
+    document.getElementById("footer").style.opacity = '0.3';
+
+})
+
+let exit = document.getElementById("exit");
+
+exit.addEventListener("click", function() {
+    document.getElementById("Register-container").style.display = 'none';
+    document.getElementById("login-page").style.opacity = '1';
+    document.getElementById("footer").style.opacity = '1';
+})
